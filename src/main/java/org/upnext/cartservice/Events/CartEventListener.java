2ad2a41f37646b647daee8a368285bcd70d89f1f
Eslam Taha <i777slam73@gmail.com>
2025-10-16 10:26:29 +0300

    Renaming

--- src/main/java/org/upnext/cartservice/Events/CartEventListener.java
+++ src/main/java/org/upnext/cartservice/Events/CartEventListener.java
@@ -3,16 +3,14 @@ package org.upnext.cartservice.Events;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
-import org.upnext.cartservice.Repositories.CartRepository;
 import org.upnext.cartservice.Services.CartService;
 import org.upnext.sharedlibrary.Dtos.SuccessfulPaymentEvent;
-import org.upnext.sharedlibrary.Events.OrderCreatedEvent;
 
 import static org.upnext.cartservice.Configurations.RabbitMqConfig.CART_CLEAR_QUEUE;
 
 @Service
 @RequiredArgsConstructor
-public class CartEventListiner {
+public class CartEventListener {
     private final CartService cartService;
     @RabbitListener(queues = CART_CLEAR_QUEUE)
     public void handleClearCart(SuccessfulPaymentEvent successfulPaymentEvent) {
