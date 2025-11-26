05b59e72bc51b56a62d7e84759b602135583635e
Eslam Taha <i777slam73@gmail.com>
2025-11-26 15:36:21 +0200

    mend

--- src/main/java/org/upnext/cartservice/Clients/ProductsClient.java
+++ src/main/java/org/upnext/cartservice/Clients/ProductsClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.upnext.sharedlibrary.Dtos.ProductDto;
 
-@FeignClient(name = "localhost:8081/")
+@FeignClient(name = "product-service")
 public interface ProductsClient {
     @GetMapping("/products/{id}")
     ProductDto getProduct(@PathVariable Long id);
